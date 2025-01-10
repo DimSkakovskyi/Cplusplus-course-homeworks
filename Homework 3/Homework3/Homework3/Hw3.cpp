170c25a4a04dbbb44814b6a0a05c2c2637811d68
DimSkakovskyi <dimonskak@ukr.net>
2025-01-10 16:47:15 +0200

    Hw3 added

---
+++ Homework 3/Homework3/Homework3/Hw3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	const float fPi = 3.14159;
+
+	float MyVariable;
+	MyVariable = fPi;
+	cout << "pi is: " << MyVariable << endl;
+
+	return 0;
+}
